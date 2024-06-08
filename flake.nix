1587a23db179f53867c8ae904790761418774f7c
Justin Restivo <justin.p.restivo@gmail.com>
2024-06-08 07:15:32 -0400

    revert to stable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    home-manager.url = github:nix-community/home-manager;
+    nixpkgs.url = github:NixOS/nixpkgs/24.05;
+    home-manager.url = github:nix-community/home-manager/release-24.05;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
