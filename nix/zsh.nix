748e46b2b4bac8de21d92bfd631cf6e081ef4fff
JachymPutta <jachym.putta@gmail.com>
2024-06-11 21:19:21 -0400

    chore: update command

--- nix/zsh.nix
+++ nix/zsh.nix
@@ -10,7 +10,8 @@
     shellAliases = {
       ll = "ls -l";
       # update = "sudo nixos-rebuild switch --flake .#jachym -L";
-      update = "sudo nix build .#nixosConfigurations.jachym.config.system.build.toplevel && sudo ./result/activate";
+      # update = "sudo nix build .#nixosConfigurations.jachym.config.system.build.toplevel && sudo ./result/activate";
+      update = "home-manager switch --flake .#jachym -L";
     };
     history = {
       size = 10000;
