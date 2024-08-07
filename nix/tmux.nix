8d667b46c0da6f0f45e7bc5ef544c8664c19eec1
Jachym Putta <jachym.putta@yale.edu>
2024-08-07 09:17:40 -0400

    feat: tmux

---
+++ nix/tmux.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    historyLimit = 1000000;
+    baseIndex = 1;
+    prefix = "C-a";
+    extraConfig = builtins.readFile ../extras/tmux/tmux.conf;
+    plugins = with pkgs.tmuxPlugins; [
+        better-mouse-mode
+        catppuccin
+        sensible
+        vim-tmux-navigator
+    ];
+  };
+}
+
