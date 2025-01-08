934847249453442d82f9d7a41867ca8d53992456
Jachym Putta <jachym.putta@yale.edu>
2025-01-08 09:34:57 -0500

    chore: remove darwin.sh

--- darwin.sh
+++
@@ -1,2 +0,0 @@
-sudo nix build  --extra-experimental-features flakes --extra-experimental-features nix-command .#darwinConfigurations.Jachyms-MacBook-Pro.system --show-trace -L || exit 1 # --show-trace
-sudo ./result/activate || exit 1
